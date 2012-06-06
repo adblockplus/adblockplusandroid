78718ca779923364c3b8d861ed12eca9430ede5c
Andrey Novikov <andrey@adblockplus.org>
2012-06-06 10:59:17 +0400

    Redesigned about dialog

--- src/org/adblockplus/android/AboutDialog.java
+++ src/org/adblockplus/android/AboutDialog.java
@@ -7,10 +7,11 @@ import java.io.InputStreamReader;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.text.Html;
-import android.text.util.Linkify;
+import android.text.method.LinkMovementMethod;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -29,32 +30,45 @@ public class AboutDialog extends Dialog
 	{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.about);
-		String versionName = null;
+		String versionName = "--";
+		int versionCode = -1;
 		try
 		{
-			versionName = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionName;
+			PackageInfo pi = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+			versionName = pi.versionName;
+			versionCode = pi.versionCode;
 		}
 		catch (NameNotFoundException ex)
 		{
-			versionName = "unable to retreive version";
 		}
 
-		TextView tv = (TextView) findViewById(R.id.legal_text);
-		tv.setText(Html.fromHtml(readRawTextFile(R.raw.legal).replace("\\n", "<br/>")));
-		Linkify.addLinks(tv, Linkify.ALL);
-		tv = (TextView) findViewById(R.id.info_text);
-		String info = "<h3>" + mContext.getString(R.string.app_name) + "</h3>" + mContext.getString(R.string.version) + ": " + versionName + "<br/><br/>" + readRawTextFile(R.raw.info);
-		tv.setText(Html.fromHtml(info));
-		Linkify.addLinks(tv, Linkify.ALL);
+		StringBuilder info = new StringBuilder();
+		info.append("<h3>");
+		info.append(mContext.getString(R.string.app_name));
+		info.append("</h3>");
+		info.append("<p>");
+		info.append(mContext.getString(R.string.version));
+		info.append(": ");
+		info.append(versionName);
+		info.append(" ");
+		info.append(mContext.getString(R.string.build));
+		info.append(" ");
+		info.append(versionCode);
+		info.append("</p>");
+		appendRawTextFile(info, R.raw.info);
+		appendRawTextFile(info, R.raw.legal);
+		
+		TextView tv = (TextView) findViewById(R.id.about_text);
+		tv.setText(Html.fromHtml(info.toString()));
+		tv.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
-	public static String readRawTextFile(int id)
+	public static void appendRawTextFile(StringBuilder text, int id)
 	{
 		InputStream inputStream = mContext.getResources().openRawResource(id);
 		InputStreamReader in = new InputStreamReader(inputStream);
 		BufferedReader buf = new BufferedReader(in);
 		String line;
-		StringBuilder text = new StringBuilder();
 		try
 		{
 			while ((line = buf.readLine()) != null)
@@ -62,8 +76,6 @@ public class AboutDialog extends Dialog
 		}
 		catch (IOException e)
 		{
-			return null;
 		}
-		return text.toString();
 	}
 }
