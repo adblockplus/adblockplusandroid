6808a49dd544fcb3753cf312e7fd46e686247983
Andrey Novikov <andrey@adblockplus.org>
2012-11-07 14:48:22 +0400

    Use 204 response code to get rid of error messages shown to users

--- src/org/adblockplus/brazil/RequestHandler.java
+++ src/org/adblockplus/brazil/RequestHandler.java
@@ -136,7 +136,7 @@ public class RequestHandler implements Handler
 
     if (block)
     {
-      request.sendError(502, "Blocked by Adblock Plus");
+      request.sendHeaders(204, null, 0);
       return true;
     }
 
